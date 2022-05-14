276d2137e86f4787ee9f0fc7f6a892c3c37eaaee
Chemi Martinez <jmmartineztuarez@gmail.com>
2022-05-14 14:45:35 -0400

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', null, 'chatapp');
+    $conn = mysqli_connect('containers-us-west-49.railway.app', 'root', 'lfEFtgDSzEdOdvoTrfCN', 'railway',6824);
     if(!$conn){
         echo "database connected" . mysqli_connect_error();
     }
